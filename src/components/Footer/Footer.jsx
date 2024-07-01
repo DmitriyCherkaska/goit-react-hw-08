f9894ebdad3ec9c565d332671a9cb91a1fd17528
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-07-02 00:04:04 +0300

    sdfsdf

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,17 +3,16 @@ import { FaLinkedin } from 'react-icons/fa';
 
 export const Footer = () => {
   return (
-    <div>
-      <footer className={style.container}>
-        <p className={style.text}>Created by Dmitriy</p>
-        <p className={style.text}>© 2024 Phonebook</p>
-        <p className={style.text}>
-          Built with HTML, CSS, JavaScript, React, Redux, Redux Persist
-        </p>
-        <p className={style.text}>
+    <div className={style.footer}>
+      <footer>
+        <p>Created by Dmitriy</p>
+        <p>© 2024 Contacts book</p>
+        <p>Built with HTML, CSS, JavaScript, React, Redux, Redux Persist</p>
+        <p className={style.linkedin}>
           Connect with me on
           <a
-            href="https://www.linkedin.com/in/dmitriy-cherkaska-3962912b0/"
+            className={style.icon}
+            href="https://www.linkedin.com/in/-inna-kushnir-"
             target="_blank"
           >
             <FaLinkedin fill="rgb(182 205 212)" size={22} />
